3b7bf47362d697eb9113158bfd6f207af95c80d8
Raistlind <johnd0712@gmail.com>
2021-08-30 22:55:41 +0800

    edit post detail

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+// const A_CONST: i32 = 42;
+
+fn avg(a: u32, b: u32) -> u32 {
+    (a & b) + ((a ^ b) >> 1)
 }
+
+
+fn main() {
+    assert_eq!(avg(4294967295, 4294967295), 4294967295);
+    assert_eq!(avg(0, 0), 0);
+    assert_eq!(avg(10, 20), 15);
+    assert_eq!(avg(4294967295, 1), 2147483648);
+    println!("passed")
+}
+
